bdaf15025b19efc542994a28dd50e7141e49fb13
Nitin <nitinkmr.4169@gmail.com>
2025-05-20 10:15:01 +0530

    Added reduce( )

--- List.py
+++ List.py
@@ -19,11 +19,33 @@ print(result)
 
 #Definition :- Filters items from an iterable based on a function that returns True or False
 
-numbers = [1, 2, 3]
+numbers = [1, 2, 3, 4, 5, 6]
 
 def isEven(n):
     return n % 2 == 0
 
-result = filter(isEven, numbers)
+result = filter(lambda n : n % 2 == 0, numbers)
 
-print(result)
+print(list(result))
+
+
+
+#REDUCE()
+
+#syntax :- function(function, iterable)
+
+#Definition :- Reduce is used to calcuate a value out of a sequence like a list or [items of an iterable to reduce it to a single value]
+
+#It's little bit different from map() or filter() where it's not available automatically we have to import it from standard library func tools
+
+from functools import reduce
+
+expenses = [
+      ('Dinner', 80),
+      ('Car repair', 120)
+]
+
+sum = reduce(lambda a, b: a[1] + b[1], expenses)
+
+print(sum)
+
