0935397af17298874090b15f9b782ecaf588fe02
Nitin <nitinkmr.4169@gmail.com>
2025-05-20 12:00:20 +0530

    Annotations

---
+++ Annotations.py
@@ -0,0 +1,14 @@
+#Annotations
+
+#Annotation in python are a way to attach metadata to function parameters and return values, usually to indicate expected data types
+
+#code
+  
+#example use in function
+
+def increment(n: int) -> int:
+  return n + 1
+
+#example use in variable
+
+count: int= 0 #we're specifying that this variable is going to be an integer
