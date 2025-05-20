c99b00eb4da066e86defd9bec03caf0bb8e86deb
Nitin <nitinkmr.4169@gmail.com>
2025-05-20 12:48:08 +0530

    How to intall and uninstall any packages

---
+++ pip.py
@@ -0,0 +1,14 @@
+#pip
+
+#How to install 3rd party packages in python using pip the python standard library
+
+
+#How to install packages
+#pip install requests  #requests is a package here
+
+
+#How to upgrade a package
+# :- pip install -u requests
+
+#uninstall package
+# :- pip uninstall requests
