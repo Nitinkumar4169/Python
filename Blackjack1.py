157350e7b27db7200e4a02b594b28fc2b4ee0abb
Nitin <nitinkmr.4169@gmail.com>
2025-05-23 19:29:19 +0530

    Blackjack game 1

---
+++ Blackjack1.py
@@ -0,0 +1,8 @@
+suits = ["spades", "clubs", "hearts", "diamonds"]
+suit = suits[2]
+rank = "K"
+value = 10
+print(" Your card is: " + rank + " of " + suit)
+suits.append("snakes")
+for suit in suits:
+  print(suit)
