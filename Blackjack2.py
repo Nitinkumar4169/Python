8eb9d8ab1928aed49b952ddb57d01a8636d51f05
Nitin <nitinkmr.4169@gmail.com>
2025-05-23 19:41:40 +0530

    52 card printed as item list

---
+++ Blackjack2.py
@@ -0,0 +1,12 @@
+#Representing a full deck of cards with python code
+
+cards = []
+suits = ["spades", "clubs", "hearts", "diamonds"]
+
+#lIST OF RANKS
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+for suit in suits:
+     for rank in ranks:
+      print([suit, rank])
+
