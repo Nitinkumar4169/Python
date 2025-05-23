d8da5cd38c15724974d1af3d40b39475f80c461b
Nitin <nitinkmr.4169@gmail.com>
2025-05-23 19:52:22 +0530

    import random and shuffle the card

--- Blackjack2.py
+++ Blackjack2.py
@@ -1,5 +1,7 @@
 #Representing a full deck of cards with python code
 
+import random
+
 cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
 
@@ -8,5 +10,7 @@ ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 for suit in suits:
      for rank in ranks:
-      print([suit, rank])
+      cards.append([suit, rank])
 
+random.shuffle(cards)
+print(cards)
